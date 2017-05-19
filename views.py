9109c1f90c54438dcd7da6876a3b7a78c0f8f717
i.chelyshkin <sir.igoss@yandex.ru>
2017-05-19 11:58:48 +0300

    update views

--- views.py
+++ views.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import get_object_or_404
 from django.shortcuts import render
+from django.http import Http404
 
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.http import Http404
 from .models import Article, Flow, Tag, TagGroup
 
 
