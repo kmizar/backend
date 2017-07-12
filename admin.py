f09bfbc8bd87f46482aee20f5a96abbe85d3c437
i.chelyshkin <sir.igoss@yandex.ru>
2017-07-12 17:08:13 +0300

    Feature #2: make count disable in django admin interface

--- admin.py
+++ admin.py
@@ -2,7 +2,16 @@
 from django.contrib import admin
 from .models import Article, Flow, Tag, TagGroup
 
+#------------------------------------------
+#Override django admin
+
+class ArticleAdmin(admin.ModelAdmin):
+    readonly_fields = ('article_count',)
+
+
+#------------------------------------------
+#Register models
 admin.site.register(Flow)
 admin.site.register(TagGroup)
 admin.site.register(Tag)
-admin.site.register(Article)
+admin.site.register(Article, ArticleAdmin)
