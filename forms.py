f27b3a046f7179ffb4554f8e10c60ddcf2e1e13b
i.chelyshkin <sir.igoss@yandex.ru>
2017-07-17 16:06:04 +0300

    Feature #6: add search from with test suggest

--- forms.py
+++ forms.py
@@ -3,16 +3,39 @@ from django import forms
 from .models import Tag
 
 #Tag searcher form
-class TagSearcher(forms.ModelForm):
-    searcher = forms.CharField(
-        label='',
-        help_text='',
-        widget=forms.TextInput(attrs={
-            'class': 'form-control',
-        })
+class TagSearcher(forms.Form):
+    OPTIONS = (
+        ("AUT", "Austria"),
+        ("DEU", "Germany"),
+        ("NLD", "Neitherlands"),
     )
 
+    searcher = forms.ChoiceField(choices=OPTIONS, required=True, label='', widget=forms.Select(attrs={'class':'js-example-basic-multiple','multiple':'multiple'}))
 
-    class Meta:
-        model   = Tag
-        fields  = ('searcher',)
+
+
+
+    #searcher = forms.ChoiceField(choices=OPTIONS)
+    #widget = forms.ChoiceField(attrs={
+    #    'class': 'js-example-basic-multiple',
+    #    'multiple': 'multiple',
+    #})
+    #widget=forms.Select(attrs={'class':'js-example-basic-multiple'})
+
+
+
+
+
+    #searcher = forms.CharField(
+    #    label='',
+    #    help_text='',
+    #    widget=forms.TextInput(attrs={
+    #        'class': 'form-control',
+    #        'autocomplete': 'off',
+    #    })
+    #)
+
+
+    #class Meta:
+    #    model   = Tag
+    #    fields  = ('searcher',)
